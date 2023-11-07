13344c66c0a76853944394d20309849514c0e2ff
qalab abbas <qasargana@gmail.com>
2023-11-07 12:27:07 +0500

    upload task 11/7/2023

---
+++ 11-07-2023/task1.js
@@ -0,0 +1,19 @@
+var score=8;
+console.log("mid-level skills:", score>0 && score<10);
+var timeRemaining=0;
+var energy=10;
+console.log("Game over :", energy==0 || timeRemaining==0);
+var num1=2;
+var num2=5;
+var test1=num1%2;
+var test2=num2%2;
+var result1=test1==0;
+var result2=test2==0;
+console.log("Is",num1, "an even number?", result1);
+console.log("Is",num2, "an even number?", result2);
+
+
+
+
+
+
