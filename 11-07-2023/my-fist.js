6f0dcfc4ab6f3a6c8fd3adf0f1e383f94c7d3bca
qalab abbas <qasargana@gmail.com>
2023-11-07 12:01:36 +0500

    javascript indutroductory 11/07/2023

---
+++ 11-07-2023/my-fist.js
@@ -0,0 +1,39 @@
+console.log("hasan");
+// var a=5;
+console.log(typeof(a));
+console.log(typeof(true));
+var a;
+console.log(typeof(a));
+
+var currentTime= 10;
+console.log(currentTime>9 && currentTime<17);
+console.log(currentTime>20 || currentTime<17);
+console.log(22 % 5);
+console.log(22 / 5);
+console.log(typeof(22 / 5));
+var petHungry = true;
+petHungry=!petHungry;
+console.log(petHungry);
+console.log(typeof(petHungry));
+console.log(5==5);
+console.log(5=="5");
+console.log(5===5);
+console.log(5==="5");
+console.log("inter"+"net");
+console.log(365+"days");
+console.log(1+"2");
+var overtime=1;
+overtime+=2;
+overtime+=1;
+overtime+=2;
+overtime+=3;
+console.log(overtime);
+var longSAtring="";
+longSAtring+="once";
+longSAtring+="upon";
+longSAtring+="a";
+
+longSAtring+="time";
+longSAtring+="....";
+console.log(longSAtring);
+
