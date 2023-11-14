440442ccb6b6c940c914f31a718311ea3d4f8bcb
qalab abbas <qasargana@gmail.com>
2023-11-14 10:31:19 +0500

    upload j.s task 3 11/14/2023

---
+++ 11-14-2023/for-loop.js
@@ -0,0 +1,14 @@
+for(var i=1; i<4; i++){
+    console.log(i);
+}
+var counter= 3;
+while(counter>0){
+    console.log(counter);
+    // counter=counter-1;
+    counter--;
+}
+for(var i=0; i<2; i++){
+    for(var j=1; j<=7; j++){
+        console.log("week" + i + "day" + j);
+    }
+}
---
+++ 11-14-2023/task3.js
@@ -0,0 +1,35 @@
+for(var i=1; i<11; i++){
+    if(i==1){
+        // console.log(i);
+        console.log("Gold medal");
+        
+    }
+    else if(i==2){
+        // console.log(i);
+     console.log("Silver medal");
+    
+    }
+    else if(i==3){
+        // console.log(i);
+        console.log("Bronze medal");
+        
+    }
+    else{
+        console.log(i);
+    }
+}
+for(var i=1; i<=10;i++){
+    switch(i){
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver medal");
+            break;
+        case 3:
+            console.log("Bronze medal");
+            break;
+      default:
+        console.log(i);        
+    }
+}
