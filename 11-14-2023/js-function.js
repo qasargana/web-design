df9586e20cc0e00d6e6133fc91e8aa1495b3c581
qalab abbas <qasargana@gmail.com>
2023-11-14 11:01:04 +0500

    upload j.s function

---
+++ 11-14-2023/js-function.js
@@ -0,0 +1,17 @@
+function addtwonums(){
+    var a=10;
+    var b=20;
+    var c=a+b;
+    console.log(c);
+}
+for(var i=1; i<4; i++){
+    addtwonums();
+}
+function addtwonum(a,b){
+    var c = a + b;
+    console.log(c);
+}
+for(var i=1; i<4; i++){
+    addtwonum(2,2);
+}
+
