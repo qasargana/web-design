e1fc777e7814eeddabf4f48cf27a1b16df9e8ec5
qalab abbas <qasargana@gmail.com>
2023-11-14 09:16:00 +0500

    upload task 1 11/14/2023

---
+++ 11-14-2023/conditional.js
@@ -0,0 +1,32 @@
+
+    var result=40;
+    if(result>40){
+        console.log("congratulation you passed.");
+    }
+    else{
+        console.log("unfortuatly you did not passed.");
+    }
+var food = "hot";
+if (food == "hot"){
+    console.log("Too hot");
+}
+else if (food=="cold"){
+console.log('Too cold');
+}
+else{
+    console.log('just right');
+}
+var place='first';
+switch(place){
+    case 'first':
+        console.log('Gold');
+        break;
+    case 'second':
+        console.log('Silver');
+        break;
+    case 'third':
+        console.log('Bronze');
+        break;
+   default:
+    console.log('No medal');       
+}
---
+++ 11-14-2023/task1.js
@@ -0,0 +1,14 @@
+var age='Qalab';
+if (age>=65){
+    console.log("you get your incomefrom your pension");
+}
+else if(age<65 && age>=18){
+    console.log("Each month you get a salary");
+
+}
+else if (age<18){
+    console.log('You get an allownce');
+}
+else{
+    console.log('the value of the variable not numerical');
+}
