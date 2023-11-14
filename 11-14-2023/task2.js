4a20956b67ba4e5c6ec6dbf5809fbc7dbfe16a49
qalab abbas <qasargana@gmail.com>
2023-11-14 09:41:35 +0500

    upload task 2 11/14/2023

---
+++ 11-14-2023/task2.js
@@ -0,0 +1,27 @@
+var day="sunday";
+switch (day){
+    case 'monday':
+        console.log('Do something');
+        break;
+    case 'tuesday':
+        console.log('Do something');
+        break;
+    case 'wednesday':
+        console.log('Do something');
+        break;
+    case 'thursday':
+    console.log('Do something');
+        break;
+    case 'friday':
+        console.log('Do something');
+        break;
+    case 'saturday':
+        console.log('Do something');
+        break;
+    case 'sunday':
+        console.log('Do something');
+        break;
+    default:
+        console.log('There is no such day');
+     
+}
