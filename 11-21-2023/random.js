5c556c5e43824e00db58d357b2eb5bdc497a1f8a
Qalab-Abbbas <70133488@student.uol.edu.pk>
2023-11-21 09:06:34 +0500

    upload file random.js

---
+++ 11-21-2023/random.js
@@ -0,0 +1,19 @@
+Math.random();
+var decimal= Math.random();
+console.log(decimal);
+console.log(decimal*10);
+var decimal = Math.random() * 10;
+var rounded = Math.ceil(decimal);
+console.log(rounded);
+var rounded = Math.trunc(decimal);
+console.log(rounded);
+var string1="ho-ho-ho";
+console.log(string1.indexOf('h'));
+console.log(string1.lastIndexOf('h'));
+var string3="ho-ho-ho";
+console.log(string3.split('-'));
+var greet="Hello,";
+console.log(greet.toUpperCase());
+console.log(greet.toLowerCase());
+
+
